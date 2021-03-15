3982bb7ed066ac93399373dec38e6141fc5f5388
lumprv <lum.pireva@student.uni-pr.edu>
2021-03-15 15:53:44 +0100

    Verifying pass with shadow file

--- login.py
+++ login.py
@@ -1,12 +1,39 @@
 import os
 from getpass import getpass
-from hashlib import sha256
+import crypt
 
-username = input("Username: ")
-password = getpass("Password: ")
+SHADOW_LOC = '/etc/shadow'
 
-if username == "admin" and password == "password":
-    print("Welcome '{}'!".format(username))
-else:
-    print("Wrong credentials!")
 
+def main():
+    if os.access(SHADOW_LOC, os.F_OK):  # Make sure Shadow file exists.
+        attempt = 0
+        while attempt < 3:
+            shadowRead = open(SHADOW_LOC, mode='r')
+            user = input("Specify User Account: ")
+            password = getpass("Type your password: ")
+            userFound = False
+            for line in shadowRead.readlines():  # Make sure user exists
+                if (user in line):
+                    userFound = True
+                    line = line.strip()  # Remove blank space
+                    line = line.replace("\n", "").split(":")
+                    if line[1] not in ['x', '*', '!']:
+                        user = line[0].strip()
+                        hashedPass = line[1].strip()
+                        hashType = hashedPass.split("$")[1]
+                        salt = hashedPass.split("$")[2]
+                        insalt = "$" + hashType + "$" + salt + "$"
+                        hash = crypt.crypt(password, insalt)
+            if userFound is True and hash == hashedPass:  # Alert if user does not exist
+                print("Welcome '{}'!".format(user))
+                break
+            else:
+                print("WRONG CREDENTIALS!")
+                attempt = attempt + 1
+    else:  # Alert if shadow file does not exist
+        print("Shadow file does not exist at "), SHADOW_LOC
+
+
+if __name__ == '__main__':
+    main()
